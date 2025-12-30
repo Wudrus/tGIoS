5734394dca10744a6e71e3526599030194d1bf45
Wudrus <woody.play.pixbox@gmail.com>
2025-12-30 20:40:33 +0300

    Quartz sync: Dec 30, 2025, 8:40 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ru-RU",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "wudrus.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
